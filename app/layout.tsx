6bcf1e543f59bdaf1f9cb4ecef7d3d12085a9437
Jumjum Ramos <jumjumramos@gmail.com>
2024-05-05 04:36:05 +0800

    some additional things

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ranting",
-  description: "Ranting and Whining Site",
+  description: "Ranting and Whining",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>HELLO RANTING.COM!!!</h1>
+        <h1 className=" font-mono justify-center">HELLO RANTING.COM!!!</h1>
         <h1>What to commit?!</h1>
         <h1>Still on Progress as I don't know what to put yet</h1>
       </div>
